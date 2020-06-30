b2c325c1cad08a7499f4e4d9cfd190db4032e75b
suvaline vana <suvaline.vana@gmail.com>
2020-06-30 16:44:27 +0300

    Updates to dada2 quality filter

--- script_folder/dada2-quality-filter.R
+++ script_folder/dada2-quality-filter.R
@@ -1,6 +1,5 @@
 a = Sys.getenv('ENV_FILE_TEST')
 library("dada2")
-getwd()
 
 if (dir.exists("/input/dada2-quality-filter-output")) {
     unlink("/input/dada2-quality-filter-output", recursive=TRUE)
@@ -8,20 +7,29 @@ if (dir.exists("/input/dada2-quality-filter-output")) {
 
 dir.create('/input/dada2-quality-filter-output')
 
-truncLen = Sys.getenv('truncLen')
-truncQ = Sys.getenv('truncQ')
-maxLen = Sys.getenv('maxLen')
-minLen = Sys.getenv('minLen')
 maxEE = Sys.getenv('maxEE')
 maxN = Sys.getenv('maxN')
+truncQ = Sys.getenv('truncQ')
+truncLen = Sys.getenv('truncLen')
+minLen = Sys.getenv('minLen')
+maxLen = Sys.getenv('maxLen')
 minQ = Sys.getenv('minQ')
 
-input = list.files(pattern = "fastq")
+x = iconv(maxEE, from = '', to = 'UTF-8')
+x
+Encoding(x)
 
-for (fastq in input)
-{
-    output = paste('/input/dada2-quality-filter-output/',fastq, sep="")
-    out <- filterAndTrim(fastq, output, rev = NULL, filt.rev = NULL, truncLen, maxN, maxEE, truncQ, minLen, minQ, maxLen)
-    print(fastq)
-    head(out)
-}
+library(stringr)
+parameters = paste(maxEE, maxN, truncQ, truncLen, minLen, maxLen, minQ, sep=',')
+parameters = str_replace_all(parameters, ',,', ',')
+Encoding(parameters)
+
+
+
+# input = list.files(pattern = "fastq")
+# output = paste('/input/dada2-quality-filter-output/',input, sep="")
+# out <- filterAndTrim(input, output, rev = NULL, filt.rev = NULL, maxEE, compress=FALSE)
+# head(out)
+
+
+# readRenviron("C:/Users/m_4_r/Desktop/materializeDEMO/pipecraft-quick-start/env_files/dada2-quality-filter.env")

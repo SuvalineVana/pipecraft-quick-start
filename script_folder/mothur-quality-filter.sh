d4c9f407ea6fd6e6e845c39621450d6f1d6f9876
suvaline vana <suvaline.vana@gmail.com>
2020-07-01 18:54:58 +0300

    minor improvements to mothur-quality-filter.sh

--- script_folder/mothur-quality-filter.sh
+++ script_folder/mothur-quality-filter.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+#unzip fastq.qz file(s) for mothur
 gunzip *fastq.gz
 
+#Set output dir and split fastq file(s) into fasta and qual file(s)
 for f in *fastq
 do
     echo "Processing" $f
@@ -9,12 +11,24 @@ do
     output='/input/mothur-quality-filter-output/'
     #logname='mothur-quality-filter-output/'$f'_log.txt'
     echo $output
-    mothur "#set.dir(output=$output);fastq.info(fastq=$input)"
+    mothur "#set.dir(output=$output);fastq.info(fastq=$input)" &
 done
 
+#move to output folder
 cd $output
 
+#trim sequences using user set parameters
 for fa in *fasta
 do 
     mothur "#trim.seqs(fasta=$fa $qwindowaverage $qwindowsize $maxambig $qthreshold $minlength $maxlength $processors)" &
 done
+
+#merge qual and fasta file(s) back into fastq file(s)
+for fa in *fasta
+do 
+    qfile=$(echo $fa | sed 's/.fasta/.qual/g')
+    echo $qfile
+    mothur "#make.fastq(fasta=$fa, qfile=$qfile)" &
+done
+
+#remove all unnecessary file(s)

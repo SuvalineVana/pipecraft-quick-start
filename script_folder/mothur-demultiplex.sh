03ebdc03c825ec1cffaaf8427d76612a368e6960
Martin Metsoja <suvaline.vana@gmail.com>
2020-11-06 11:54:18 +0200

    6.11 minor updates to front-end

--- script_folder/mothur-demultiplex.sh
+++ script_folder/mothur-demultiplex.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 start=`date +%s`
 
+
 echo "This is Mothur Demulitplexer pre-script"
 echo $bdiffs
 echo $pdiffs
+echo $tdiffs
 echo $min_unique_size
 echo $fwdPrimers
 echo $revPrimers
@@ -11,6 +13,9 @@ echo $inputFilesArray
 echo $oligosFile
 echo $seqTech
 echo $reOrient
+
+
+
 end=`date +%s`
 runtime=$((end-start))
 echo $runtime
